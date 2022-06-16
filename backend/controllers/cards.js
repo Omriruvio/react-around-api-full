7c459366d8e5601eac5269c19c375a60086f08b0
Omri Ruvio <omriruvio@gmail.com>
2022-06-16 18:37:29 +0300

    refactor: central err handling, rate limit, linter

--- backend/controllers/cards.js
+++ backend/controllers/cards.js
@@ -1,57 +1,36 @@
 const Card = require('../models/card');
-const ValidationError = require('../utils/validationerror');
 const NotFoundError = require('../utils/notfounderror');
-const { NOT_FOUND, BAD_REQUEST, FORBIDDEN } = require('../utils/httpstatuscodes');
-const sendDefaultError = require('../utils/senddefaulterror');
 const AuthorizationError = require('../utils/authorizationerror');
 
-const getCards = (req, res) => {
+const getCards = (req, res, next) => {
   Card.find({})
     .orFail(() => {
       throw new NotFoundError('Card list is empty.');
     })
     .then((cards) => res.send(cards))
-    .catch((err) => {
-      if (err instanceof NotFoundError) {
-        res.status(NOT_FOUND).send({ message: err.message });
-      } else sendDefaultError(res, err);
-    });
+    .catch(next);
 };
 
-const createCard = (req, res) => {
+const createCard = (req, res, next) => {
   const { name, link } = req.body;
   Card.create({ name, link, owner: req.user._id })
     .then((card) => res.send(card))
-    .catch((err) => {
-      if (err.name === 'ValidationError') {
-        const error = new ValidationError('Invalid card data received.');
-        res.status(BAD_REQUEST).send({ message: error.message });
-      } else sendDefaultError(res, err);
-    });
+    .catch(next);
 };
 
-const deleteCard = (req, res) => {
+const deleteCard = (req, res, next) => {
   Card.findOne({ _id: req.params.cardId })
     .then((card) => {
       if (!card) throw new NotFoundError('Card not found.');
-      if (card.owner != req.user._id) throw new AuthorizationError('Unaothorized card deletion request', 403);
+      if (String(card.owner) !== req.user._id) throw new AuthorizationError('Unaothorized card deletion request', 403);
       return Card.findByIdAndRemove(req.params.cardId).then((deletedCard) => {
         res.send(deletedCard);
       });
     })
-    .catch((err) => {
-      if (err instanceof AuthorizationError) {
-        res.status(FORBIDDEN).send({ message: err.message });
-      } else if (err.name === 'CastError') {
-        const error = new ValidationError('Invalid card data received.');
-        res.status(BAD_REQUEST).send({ message: error.message });
-      } else if (err instanceof NotFoundError) {
-        res.status(NOT_FOUND).send({ message: err.message });
-      } else sendDefaultError(res, err);
-    });
+    .catch(next);
 };
 
-const likeCard = (req, res) => {
+const likeCard = (req, res, next) => {
   const { cardId } = req.params;
   const { _id: userId } = req.user;
   Card.findByIdAndUpdate(cardId, { $addToSet: { likes: userId } }, { new: true })
@@ -59,17 +38,10 @@ const likeCard = (req, res) => {
       throw new NotFoundError('Card id not found.');
     })
     .then((card) => res.send(card))
-    .catch((err) => {
-      if (err.name === 'CastError') {
-        const error = new ValidationError('Invalid card id received.');
-        res.status(BAD_REQUEST).send({ message: error.message });
-      } else if (err instanceof NotFoundError) {
-        res.status(NOT_FOUND).send({ message: err.message });
-      } else sendDefaultError(res, err);
-    });
+    .catch(next);
 };
 
-const removeLike = (req, res) => {
+const removeLike = (req, res, next) => {
   const { cardId } = req.params;
   const { _id: userId } = req.user;
   Card.findByIdAndUpdate(cardId, { $pull: { likes: userId } }, { new: true })
@@ -77,14 +49,7 @@ const removeLike = (req, res) => {
       throw new NotFoundError('Card id not found.');
     })
     .then((card) => res.send(card))
-    .catch((err) => {
-      if (err.name === 'CastError') {
-        const error = new ValidationError('Invalid card id received.');
-        res.status(BAD_REQUEST).send({ message: error.message });
-      } else if (err instanceof NotFoundError) {
-        res.status(NOT_FOUND).send({ message: err.message });
-      } else sendDefaultError(res, err);
-    });
+    .catch(next);
 };
 
 module.exports = {
--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -1,15 +1,14 @@
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const User = require('../models/user');
-const sendDefaultError = require('../utils/senddefaulterror');
 const ValidationError = require('../utils/validationerror');
 const NotFoundError = require('../utils/notfounderror');
 const AuthorizationError = require('../utils/authorizationerror');
-const { NOT_FOUND, BAD_REQUEST, UNAUTHORIZED, REQUEST_CONFLICT } = require('../utils/httpstatuscodes');
+const ConflictError = require('../utils/conflicterror');
 
 const { JWT_SECRET, NODE_ENV } = process.env;
 
-const getCurrentUser = (req, res) => {
+const getCurrentUser = (req, res, next) => {
   User.findById(req.user._id)
     .orFail(() => {
       throw new NotFoundError('User id not found.');
@@ -17,68 +16,46 @@ const getCurrentUser = (req, res) => {
     .then((user) => {
       res.send(user);
     })
-    .catch((err) => {
-      if (err.name === 'CastError') {
-        const error = new ValidationError('Invalid user id received.');
-        res.status(BAD_REQUEST).send({ message: error.message });
-      } else if (err instanceof NotFoundError) {
-        res.status(NOT_FOUND).send({ message: err.message });
-      } else sendDefaultError(res, err);
-    });
+    .catch(next);
 };
 
-const login = (req, res) => {
+const login = (req, res, next) => {
   const { email, password } = req.body;
   User.findOne({ email })
     .select('+password')
     .orFail(() => new AuthorizationError('Incorrect email or password.'))
-    .then((user) =>
+    .then((user) => {
       bcrypt.compare(password, user.password).then((match) => {
         if (!match) {
           throw new AuthorizationError('Incorrect email or password.');
         }
         const token = jwt.sign({ _id: user._id }, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret', { expiresIn: '7d' });
         res.send({ token });
-      })
-    )
-    .catch((err) => {
-      if (err instanceof AuthorizationError) {
-        res.status(UNAUTHORIZED).send({ message: err.message });
-      } else sendDefaultError(res, err);
-    });
+      });
+    })
+    .catch(next);
 };
 
-const getUsers = (req, res) => {
+const getUsers = (req, res, next) => {
   User.find({})
     .orFail(() => {
       throw new NotFoundError('User list is empty.');
     })
     .then((users) => res.send(users))
-    .catch((err) => {
-      if (err instanceof NotFoundError) {
-        res.status(NOT_FOUND).send({ message: err.message });
-      } else sendDefaultError(res, err);
-    });
+    .catch(next);
 };
 
-const getUser = (req, res) => {
+const getUser = (req, res, next) => {
   const { id } = req.params;
   User.findById(id)
     .orFail(() => {
       throw new NotFoundError('User id not found.');
     })
     .then((user) => res.send(user))
-    .catch((err) => {
-      if (err.name === 'CastError') {
-        const error = new ValidationError('Invalid user id received.');
-        res.status(BAD_REQUEST).send({ message: error.message });
-      } else if (err instanceof NotFoundError) {
-        res.status(NOT_FOUND).send({ message: err.message });
-      } else sendDefaultError(res, err);
-    });
+    .catch(next);
 };
 
-const createUser = (req, res) => {
+const createUser = (req, res, next) => {
   // prettier-ignore
   const {
     name,
@@ -100,16 +77,14 @@ const createUser = (req, res) => {
         res.send(user);
       })
       .catch((err) => {
-        if (err.code === 11000) res.status(REQUEST_CONFLICT).send({ message: err.message });
-        if (err.name === 'ValidationError') {
-          const error = new ValidationError('Invalid user data received.');
-          res.status(BAD_REQUEST).send({ message: error.message });
-        } else sendDefaultError(res, err);
+        if (err.code === 11000) {
+          next(new ConflictError('User already exists.'));
+        } else next(err);
       });
   });
 };
 
-const updateUser = (req, res) => {
+const updateUser = (req, res, next) => {
   const { name, about } = req.body;
   const { _id: id } = req.user;
   User.findByIdAndUpdate(id, { name, about }, { new: true, runValidators: true })
@@ -117,17 +92,10 @@ const updateUser = (req, res) => {
       throw new NotFoundError('User id not found.');
     })
     .then((user) => res.send(user))
-    .catch((err) => {
-      if (err.name === 'CastError') {
-        const error = new ValidationError('Invalid user id received.');
-        res.status(BAD_REQUEST).send({ message: error.message });
-      } else if (err instanceof NotFoundError) {
-        res.status(NOT_FOUND).send({ message: err.message });
-      } else sendDefaultError(res, err);
-    });
+    .catch(next);
 };
 
-const updateUserAvatar = (req, res) => {
+const updateUserAvatar = (req, res, next) => {
   const { avatar } = req.body;
   const { _id: id } = req.user;
   User.findByIdAndUpdate(id, { avatar }, { new: true, runValidators: true })
@@ -135,14 +103,7 @@ const updateUserAvatar = (req, res) => {
       throw new NotFoundError('User id not found.');
     })
     .then((user) => res.send(user))
-    .catch((err) => {
-      if (err.name === 'CastError') {
-        const error = new ValidationError('Invalid user id received.');
-        res.status(BAD_REQUEST).send({ message: error.message });
-      } else if (err instanceof NotFoundError) {
-        res.status(NOT_FOUND).send({ message: err.message });
-      } else sendDefaultError(res, err);
-    });
+    .catch(next);
 };
 
 module.exports = {
